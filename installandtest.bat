e5d79e65610db5b35e25a6cae0a51c51b4047a7b
chiawei chang <williamcw.chang@moxa.com>
2020-09-03 12:55:28 +0800

    init

---
+++ installandtest.bat
@@ -0,0 +1,3 @@
+python3 -m pip install norwegian-forest-cat
+python3 -m pip install --upgrade norwegian-forest-cat
+python3 tests/hello.py
---
+++ tests/hello.py
@@ -0,0 +1,5 @@
+from cat_pkg import cat_method
+
+
+cat = cat_method.Cat("poyo")
+print(cat.get_name())
